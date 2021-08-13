9f388992c39362d3b4c2accfb9aa7122aafc6cb0
haqh310 <haqh310@gmail.com>
2021-08-13 15:16:26 +0700

    create component export-bill-destroy

--- pharmacy-management-frontend/src/app/management/warehouse/warehouse-export/warehouse-export-routing.module.ts
+++ pharmacy-management-frontend/src/app/management/warehouse/warehouse-export/warehouse-export-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ExportBillDestroyComponent} from "./export-bill-destroy/export-bill-destroy.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'export-bill-destroy',
+    component: ExportBillDestroyComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- pharmacy-management-frontend/src/app/management/warehouse/warehouse-export/warehouse-export.module.ts
+++ pharmacy-management-frontend/src/app/management/warehouse/warehouse-export/warehouse-export.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { WarehouseExportRoutingModule } from './warehouse-export-routing.module';
+import { ExportBillDestroyComponent } from './export-bill-destroy/export-bill-destroy.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ExportBillDestroyComponent],
   imports: [
     CommonModule,
     WarehouseExportRoutingModule
