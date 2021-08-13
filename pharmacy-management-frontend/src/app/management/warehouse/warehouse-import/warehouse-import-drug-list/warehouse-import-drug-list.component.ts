e78251276aeff7a8102e9df41d485f8d9b3c29d0
hoangphamdong <hpdongbmt@gmail.com>
2021-08-13 16:09:23 +0700

    commit giao diện danh sach hoa don nhap

---
+++ pharmacy-management-frontend/src/app/management/warehouse/warehouse-import/warehouse-import-drug-list/warehouse-import-drug-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warehouse-import-drug-list',
+  templateUrl: './warehouse-import-drug-list.component.html',
+  styleUrls: ['./warehouse-import-drug-list.component.css']
+})
+export class WarehouseImportDrugListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
