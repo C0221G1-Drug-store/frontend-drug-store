6e53e2401f3e20a25db9dcaf91acd7a9c63309de
truongnhat2102 <truongnhat2102@gmail.com>
2021-08-13 16:01:19 +0700

    update NhatTV 13/08/2021

---
+++ src/app/export-bill/export-bill-list/export-bill-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-export-bill-list',
+  templateUrl: './export-bill-list.component.html',
+  styleUrls: ['./export-bill-list.component.css']
+})
+export class ExportBillListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
