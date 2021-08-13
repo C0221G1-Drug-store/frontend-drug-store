755ab6e9c0246cfd058d1d11949da798b3185ad6
Huynh Nguyen <huynhnguyen@Huynhs-MacBook-Pro.local>
2021-08-13 15:09:59 +0700

    HuynhNH created components in client

--- pharmacy-management-frontend/src/app/client/client.module.ts
+++ pharmacy-management-frontend/src/app/client/client.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ClientRoutingModule } from './client-routing.module';
+import { HomepageComponent } from './homepage/homepage.component';
+import { SearchPageComponent } from './search-page/search-page.component';
+import { CartComponent } from './cart/cart.component';
+import { DrugDetailsComponent } from './drug-details/drug-details.component';
+import { LoginRegisterComponent } from './login-register/login-register.component';
+import { HeaderFooterClientComponent } from './header-footer-client/header-footer-client.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HomepageComponent, SearchPageComponent, CartComponent, DrugDetailsComponent, LoginRegisterComponent, HeaderFooterClientComponent],
   imports: [
     CommonModule,
     ClientRoutingModule
---
+++ pharmacy-management-frontend/src/app/client/header-footer-client/header-footer-client.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-footer-client',
+  templateUrl: './header-footer-client.component.html',
+  styleUrls: ['./header-footer-client.component.css']
+})
+export class HeaderFooterClientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
