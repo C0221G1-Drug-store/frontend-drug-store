6abdd69777ac7a164a39bf3665ade665b3410924
Nhật Long Hồ <honhatlongg@gmail.com>
2021-08-13 15:54:29 +0700

    front-end

--- pharmacy-management-frontend/src/app/management/management-information/management-information.module.ts
+++ pharmacy-management-frontend/src/app/management/management-information/management-information.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ManagementInformationRoutingModule } from './management-information-routing.module';
+import { CustomerComponent } from './customer/customer.component';
+import { CreateCustomerComponent } from './customer/create-customer/create-customer.component';
+import { EditCustomerComponent } from './customer/edit-customer/edit-customer.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CustomerComponent, CreateCustomerComponent, EditCustomerComponent],
   imports: [
     CommonModule,
     ManagementInformationRoutingModule
