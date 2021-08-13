e4afb37edecd108008ebbfb5552f24b2933bf1be
Minh Đức Đoàn <you@example.com>
2021-08-13 14:04:35 +0700

    DucDM update frontend frist time

---
+++ pharmacy-management-frontend/src/app/management/wholesale/wholesale.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { WholesaleRoutingModule } from './wholesale-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    WholesaleRoutingModule
+  ]
+})
+export class WholesaleModule { }
