1d70849b0e75e21c4697530d37112e84e9f8856e
tuan90dn <tuanql90@gmail.com>
2021-08-14 21:05:27 +0700

    update list

--- pharmacy-management-frontend/src/app/management/warehouse/drug/drug.module.ts
+++ pharmacy-management-frontend/src/app/management/warehouse/drug/drug.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { DrugRoutingModule } from './drug-routing.module';
 import { DrugCreateComponent } from './drug-create/drug-create.component';
 import { DrugEditComponent } from './drug-edit/drug-edit.component';
+import { DrugListComponent } from './drug-list/drug-list.component';
 
 
 @NgModule({
-  declarations: [DrugCreateComponent, DrugEditComponent],
+  declarations: [DrugCreateComponent, DrugEditComponent, DrugListComponent],
   imports: [
     CommonModule,
     DrugRoutingModule
