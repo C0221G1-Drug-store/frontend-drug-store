f4a9702c47affc48ff4042451ea53e8d06ca1d4e
PhanGiaKhanh <phangiakhanh90@gmail.com>
2021-08-15 04:35:30 +0700

    up

--- pharmacy-management-frontend/src/app/client/client.module.ts
+++ pharmacy-management-frontend/src/app/client/client.module.ts
@@ -1,31 +1,27 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { ClientRoutingModule } from './client-routing.module';
-import { HomepageComponent } from './homepage/homepage.component';
-import { SearchPageComponent } from './search-page/search-page.component';
-import { CartComponent } from './cart/cart.component';
-import { DrugDetailsComponent } from './drug-details/drug-details.component';
-import { LoginRegisterComponent } from './login-register/login-register.component';
-<<<<<<< HEAD
-import { HeaderFooterClientComponent } from './header-footer-client/header-footer-client.component';
+import {ClientRoutingModule} from './client-routing.module';
+import {HomepageComponent} from './homepage/homepage.component';
+import {SearchPageComponent} from './search-page/search-page.component';
+import {CartComponent} from './cart/cart.component';
+import {DrugDetailsComponent} from './drug-details/drug-details.component';
+import {LoginRegisterComponent} from './login-register/login-register.component';
 import {ReactiveFormsModule} from '@angular/forms';
-=======
-import { HeaderClientComponent } from './header-client/header-client.component';
-import { FooterClientComponent } from './footer-client/footer-client.component';
->>>>>>> bc98acdbd7094dc9546bd735d9ebe172df5a3545
+import {HeaderClientComponent} from './header-client/header-client.component';
+import {FooterClientComponent} from './footer-client/footer-client.component';
 
 
 @NgModule({
-  declarations: [HomepageComponent, SearchPageComponent, CartComponent, DrugDetailsComponent, LoginRegisterComponent, HeaderClientComponent, FooterClientComponent],
+  declarations: [
+    HomepageComponent, SearchPageComponent, CartComponent, DrugDetailsComponent,
+    LoginRegisterComponent, HeaderClientComponent, FooterClientComponent
+  ],
   imports: [
-<<<<<<< HEAD
     CommonModule,
     ClientRoutingModule,
     ReactiveFormsModule
-=======
-    ClientRoutingModule
->>>>>>> bc98acdbd7094dc9546bd735d9ebe172df5a3545
   ]
 })
-export class ClientModule { }
+export class ClientModule {
+}
