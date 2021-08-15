bf40c3647c576d94f4c15555452c73d287ed44a8
tuan90dn <tuanql90@gmail.com>
2021-08-15 22:17:06 +0700

    update interface drug

--- pharmacy-management-frontend/src/app/model/drug.ts
+++ pharmacy-management-frontend/src/app/model/drug.ts
@@ -1,5 +1,19 @@
 export interface Drug {
-  id?: number;
-  name?: string;
-  price?: number;
+  drugId?: number;
+  drugCode?: string;
+  drugName?: string;
+  drugIngredient?: string;
+  activeElement?: string;
+  drugAmount?: number;
+  unit?: string;
+  conversionUnit?: string;
+  conversionRate?: string;
+  wholesaleProfitRate?: number;
+  retailProfitRate?: number;
+  manufacturer?: string;
+  origin?: string;
+  note?: string;
+  drugSideEffect?: string;
+  drugGroup?: number;
+  flag?: boolean;
 }
