c86eaa89716352ec2f1bdf8500217a0c357fc8ef
onlinevn113 <onlinevn@gmail.com>
2021-08-16 22:32:05 +0700

    update 16/8

--- pharmacy-management-frontend/src/app/management/report/report-detail/report.component.ts
+++ pharmacy-management-frontend/src/app/management/report/report-detail/report.component.ts
@@ -17,43 +17,51 @@ export class ReportComponent implements OnInit {
   eTime: string = '';
   msgChoice: string;
   msgDate: string;
-
+  displayDate = true;
 
   constructor(private sv: ReportService) {
   }
 
   ngOnInit(): void {
-
-    console.log(Date.parse('2021-07-28'));
-    console.log(Date.parse('2021-07-27'));
-
   }
 
-  exportExcel() {
+  showDate() {
+    this.displayDate = true;
+  }
+
+  hiddenDate() {
+    this.displayDate = false;
+  }
+
+  exportExcelHaveDate() {
     if (this.sDate == '' || this.eDate == '' || this.sTime == '' || this.eTime == '') {
-      this.msgDate = 'Vui chọn ngày giờ';
+      this.msgDate = 'Vui lòng chọn ngày giờ';
       return;
-    } else if (Date.parse(this.sDate) > Date.parse(this.eDate)) {
+    }
+    if (Date.parse(this.sDate) > Date.parse(this.eDate)) {
       this.msgDate = 'Vui lòng chọn ngày bắt đầu trước ngày kết thúc';
-      return;
-    } else {
-      this.msgDate = '';
       return;
     }
     if (this.choice == '') {
       this.msgChoice = 'Vui lòng chọn loại báo cáo';
       return;
-    } else {
-      this.msgChoice = '';
-      return;
     }
-
     let startDate = this.sDate + 'T' + this.sTime;
     let endDate = this.eDate + 'T' + this.eTime;
     this.sv.importDetails(this.choice, startDate, endDate).subscribe(r => {
       this.reports = r;
       this.sv.exportExcel(this.reports, this.choice);
+      this.msgDate = '';
+    }, e=>{
+      this.msgChoice='Không tìm thấy dữ liệu';
     });
+  }
+
+  exportExcelNoDate() {
+    if (this.choice == '') {
+      this.msgChoice = 'Vui lòng chọn loại báo cáo';
+      return;
+    }
   }
 
 
