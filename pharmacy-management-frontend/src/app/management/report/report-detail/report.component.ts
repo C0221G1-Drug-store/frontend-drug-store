6cab3934d60413e5534a0212aac0b39301e11887
onlinevn113 <onlinevn@gmail.com>
2021-08-17 22:28:34 +0700

    update 17/8

--- pharmacy-management-frontend/src/app/management/report/report-detail/report.component.ts
+++ pharmacy-management-frontend/src/app/management/report/report-detail/report.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ReportService} from '../../../service/report.service';
-import {Report} from '../../../model/report';
+
 
 
 @Component({
@@ -9,12 +9,11 @@ import {Report} from '../../../model/report';
   styleUrls: ['./report.component.css']
 })
 export class ReportComponent implements OnInit {
-  reports: Report[] = [];
-  choice: string = '';
-  sDate: string = '';
-  eDate: string = '';
-  sTime: string = '';
-  eTime: string = '';
+  choice = '';
+  sDate = '';
+  eDate = '';
+  sTime = '';
+  eTime = '';
   msgChoice: string;
   msgDate: string;
   displayDate = true;
@@ -34,45 +33,42 @@ export class ReportComponent implements OnInit {
   }
 
   exportExcelHaveDate() {
-    if (this.sDate == '' || this.eDate == '' || this.sTime == '' || this.eTime == '') {
+    if ((this.sDate === '' || this.eDate === '' || this.sTime === '' || this.eTime === '') && this.displayDate) {
       this.msgDate = 'Vui lòng chọn ngày giờ';
       return;
     }
-    if (Date.parse(this.sDate) > Date.parse(this.eDate)) {
+    if ((Date.parse(this.sDate) > Date.parse(this.eDate) && this.displayDate)) {
       this.msgDate = 'Vui lòng chọn ngày bắt đầu trước ngày kết thúc';
       return;
     }
-    if (this.choice == '') {
+    if (this.choice === '') {
       this.msgChoice = 'Vui lòng chọn loại báo cáo';
       return;
     }
-    let startDate = this.sDate + 'T' + this.sTime;
-    let endDate = this.eDate + 'T' + this.eTime;
+    const startDate = this.sDate + 'T' + this.sTime;
+    const endDate = this.eDate + 'T' + this.eTime;
     this.sv.importDetails(this.choice, startDate, endDate).subscribe(r => {
-      this.reports = r;
-      this.sv.exportExcel(this.reports, this.choice);
-      this.msgDate = '';
-    }, e=>{
-      this.msgChoice='Không tìm thấy dữ liệu';
-    });
-  }
+      if (r == null) {
+        this.msgChoice = 'Không tìm thấy dữ liệu (null)';
+        return;
+      }
 
-  exportExcelNoDate() {
-    if (this.choice == '') {
-      this.msgChoice = 'Vui lòng chọn loại báo cáo';
-      return;
-    }
+      this.sv.exportExcel(r, this.choice);
+      this.msgDate = '';
+    }, e => {
+      this.msgChoice = 'Không tìm thấy dữ liệu (error)';
+    });
   }
 
 
   subDate(dateTime: string) {
-    let v = dateTime.substr(0, 10);
+    const v = dateTime.substr(0, 10);
     console.log(v);
     return v;
   }
 
   subTime(dateTime: string) {
-    let v = dateTime.substr(11, 16);
+    const v = dateTime.substr(11, 16);
     console.log(v);
     return v;
   }
---
+++ pharmacy-management-frontend/src/app/model/statistical-chart.ts
@@ -0,0 +1,3 @@
+export interface StatisticalChart {
+  doanhThu?: string;
+}
