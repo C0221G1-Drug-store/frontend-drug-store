39c2839fb05301aed6f262cc72aee75cccd9f536
Huynh Nguyen <huynhnguyen@Huynhs-MacBook-Pro.local>
2021-08-18 10:45:03 +0700

    HuynhNH updated pagination for search page

--- pharmacy-management-frontend/src/app/client/client.module.ts
+++ pharmacy-management-frontend/src/app/client/client.module.ts
@@ -14,6 +14,7 @@ import {GroupComponent} from './view-by-drug-group/group.component';
 import { LocationComponent } from './location/location.component';
 import { HealthTipsComponent } from './health-tips/health-tips.component';
 import { HotDealsComponent } from './hot-deals/hot-deals.component';
+import {NgxPaginationModule} from 'ngx-pagination';
 
 
 
@@ -31,7 +32,8 @@ import { HotDealsComponent } from './hot-deals/hot-deals.component';
     CommonModule,
     ClientRoutingModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    NgxPaginationModule
   ]
 })
 export class ClientModule { }
