94e961a33e41125e2a258f63aa36064443e1782c
PhanGiaKhanh <phangiakhanh90@gmail.com>
2021-08-22 15:43:46 +0700

    up cart

--- pharmacy-management-frontend/src/app/client/homepage/homepage.component.ts
+++ pharmacy-management-frontend/src/app/client/homepage/homepage.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Drug} from '../../model/drug';
 import {DrugGroup} from '../../model/drug-group';
 import {DrugClientService} from '../../service/drug-client.service';
 import {DrugGroupClientService} from '../../service/drug-group-client.service';
 import {Router} from '@angular/router';
+import {ToastrService} from "ngx-toastr";
+import {DrugCart} from "../../model/cart/drug-cart";
+// ADD DRUG IN CART
+const CART_KEY = 'drug-cart-id';
 
 @Component({
   selector: 'app-homepage',
@@ -12,30 +16,32 @@ import {Router} from '@angular/router';
 })
 export class HomepageComponent implements OnInit {
   drugs: Drug[] = [];
+  drugCart: DrugCart;
+  drugCartList: DrugCart[] = [];
   drugGroups: DrugGroup[] = [];
   config: any;
   data = '';
-
-  constructor(private drugService: DrugClientService, private drugGroupService: DrugGroupClientService, private router: Router) {
+  total = 0;
+  constructor(private drugService: DrugClientService, private drugGroupService: DrugGroupClientService, private router: Router, private toastrService: ToastrService) {
     this.config = {
       itemsPerPage: 3,
       currentPage: 1
     };
-    const state = this.router.getCurrentNavigation().extras.state as {data: string};
+    const state = this.router.getCurrentNavigation().extras.state as { data: string };
     if (state != null) {
       this.data = state.data;
     }
   }
 
   getDrugByDrugGroup(drugGroupName) {
-    return this.drugs.filter(drug=>drug.drugGroup == drugGroupName);
+    return this.drugs.filter(drug => drug.drugGroup == drugGroupName);
   }
-
 
 
   ngOnInit(): void {
     this.getAllDrugGroup();
     this.getAllDrug();
+    // localStorage.removeItem(CART_KEY);
   }
 
   getAllDrug() {
@@ -51,4 +57,65 @@ export class HomepageComponent implements OnInit {
     });
   }
 
+  //#region ADD DRUG IN CART
+  addCart(drugId: number, drugName: string) {
+    this.drugCartList = JSON.parse(localStorage.getItem(CART_KEY));
+    this.drugService.findDrugCartById(drugId).subscribe(drug => {
+      let flag = false;
+      console.log(this.drugCartList);
+      if (!this.drugCartList) {
+        this.drugCartList = [];
+        this.addDrugCart(drugId, 1);
+        this.drugCartList.push(this.drugCart);
+      } else {
+        for (let i = 0; i < this.drugCartList.length; i++) {
+          if (this.drugCartList[i].drugId == drugId) {
+            if (this.drugCartList[i].count >= drug.drugAmount) {
+              this.showMessageError(drugName);
+              return;
+            }
+            this.addDrugCart(drugId, this.drugCartList[i].count + 1);
+            this.drugCartList[i] = this.drugCart;
+            flag = true;
+          }
+        }
+        if (!flag) {
+          this.addDrugCart(drugId, 1);
+          this.drugCartList.push(this.drugCart);
+        }
+      }
+      console.log(this.drugCartList);
+      this.showMessageSuccess(drugName);
+      localStorage.removeItem(CART_KEY);
+      localStorage.setItem(CART_KEY, JSON.stringify(this.drugCartList));
+    });
+  }
+
+  addDrugCart(drugID: number, count: number) {
+    this.drugCart = {
+      drugId: drugID,
+      count: count,
+      drugName: '',
+      wholesalePrice: 0,
+      drugAmount: 0,
+      drugImageDetails: 0,
+      price: 0,
+    }
+  }
+
+  showMessageSuccess(drugName: string) {
+    this.toastrService.success('Thêm thành công ' + drugName, 'Thông báo', {
+      timeOut: 1000,
+      progressBar: true,
+    });
+  }
+
+  showMessageError(drugName: string) {
+    this.toastrService.error('Thuốc ' + drugName + ' đã hết hàng', 'Thông báo', {
+      timeOut: 1000,
+      progressBar: true,
+    });
+  }
+
+  //#endregion
 }
--- pharmacy-management-frontend/src/app/service/cart.service.ts
+++ pharmacy-management-frontend/src/app/service/cart.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {DrugCartBackend} from "../model/cart/drug-cart-backend";
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +10,19 @@ export class CartService {
   // API_URL = 'http://api.exchangeratesapi.io/v1/latest?access_key=9dae1030427407802f422bf0524ac146';
   API_URL = '';
   API_EMAIL = 'http://localhost:8080/email/send';
+  API_BACKEND = 'http://localhost:8080/drug/cart';
   constructor(private http: HttpClient) {
   }
 
   convertUsdCurrency(): Observable<any> {
-    return this.http.get(this.API_URL);
+    return this.http.get<any>(this.API_URL);
   }
 
   sendEmail(){
       return  this.http.get(this.API_EMAIL);
+  }
+
+  findDrugCartById(id: number): Observable<DrugCartBackend> {
+    return this.http.get<DrugCartBackend>(`${this.API_BACKEND}/${id}`)
   }
 }
