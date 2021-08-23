57276d4fe08f0ce3a93ab18504f86317785d636c
haqh310 <haqh310@gmail.com>
2021-08-23 18:02:55 +0700

    update code

--- pharmacy-management-frontend/src/app/management/warehouse/warehouse-export/custom-currency.pipe.ts
+++ pharmacy-management-frontend/src/app/management/warehouse/warehouse-export/custom-currency.pipe.ts
@@ -3,16 +3,12 @@ import{Pipe,PipeTransform} from '@angular/core';
 @Pipe({name:'currencyVND'})
 export class CustomCurrencyPipe implements PipeTransform {
   transform(items: number): string {
-    let item  = items.toString().split("");
-    let result="";
-    let j=0;
-    for(let i = item.length -1 ;i >=0 ;i--){
-      if(j% 3 ==0 && j !=0){
-        result = ","+result;
-      }
-      result = item[i] + result;
-      j++;
-    }
-    return result +" VND";
+    const formatter = new Intl.NumberFormat('it-IT', {
+      style: 'currency',
+      currency: 'VND',
+      maximumFractionDigits: 5
+    });
+    return formatter.format(items);
+    // return items.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,') + ' VND';
   }
 }
